3bb3582baea4b87749071c03cbfaf43bec3f37d0
ytyaru <yttry0@gmail.com>
2024-10-05 16:45:07 +0900

    add 1

---
+++ docs/1/js/ol-select.js
@@ -0,0 +1,71 @@
+;(function(){
+class OlSelect extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        console.log('connectedCallback:', this)
+        console.log(this.querySelectorAll('li'))
+        this._shadowRoot = this.attachShadow({mode:'open'});
+        console.log(this.querySelectorAll('li'))
+        console.log(this._shadowRoot.querySelectorAll('li'))
+        console.log(document.querySelectorAll('ol-select > li'))
+        console.log(this.#makeStyle())
+        this._shadowRoot.appendChild(this.#makeStyle())
+        this._shadowRoot.appendChild(this.#makeUl())
+        //this._shadowRoot.innerHTML = `<style>ul{list-style:none;padding:0;}</style>`
+    }
+    disconnectedCallback() {
+        console.log('disconnectedCallback')
+
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+
+        console.log('attributeChangedCallback:', name, oldValue, newValue)
+    }
+    static get observedAttributes() { return [] }
+    adoptedCallback() {
+        console.log('adoptedCallback')
+    }
+    #render() {
+        
+    }
+    #makeUl() { return this.#makeEl('ul', {}, this.#makeEl('slot', {name:'item'})) }
+    /*
+    #makeUl() {
+        this.querySelectorAll('li')
+        this.#makeEl('ul')
+        this._shadowRoot.appendChild()
+        const ul = document.createElement('ul')
+        return 
+    }
+    */
+    #makeEl(name, attrs, children) {
+        console.log(name, attrs, children)
+        const el = document.createElement(name)
+        for (let [k,v] of Object.entries(attrs ?? {})) {
+            if ('class'===k) {this.#addClass(el, v)}
+            else {el[k] = v}
+        }
+        this.#addChildren(el, children)
+        return el
+    }
+    #addClass(el, v) {
+        if ('string'===typeof v || v instanceof String) {v.split(' ').map(c=>el.classList.add(c))}
+        else if(Array.isArray(v) && v.every(V=>'string'===typeof V || V instanceof String)){v.map(V=>el.classList.add(V))}
+        else{throw new TypeError(`vは文字列かその配列であるべきです。`)}
+    }
+    #addChildren(el, v) {
+        if (null===el || undefined===el){return}
+        else if(Array.isArray(v)){el.append(...v)}
+        else{el.append(v)}
+    }
+    #makeStyle() { return this.#makeEl('style', null, `ul{list-style:none;padding:0;}`) }
+//    #makeStyle() {
+//        const style = this.#makeEl('style', null, `ul{list-style:none;padding:0;}`)
+//        style.textContent = `ul{list-style:none;padding:0;}`
+//        retur
+//    }
+}
+customElements.define('ol-select', OlSelect);
+})();
---
+++ docs/1/js/ui-chooser.js
@@ -0,0 +1,49 @@
+;(function(){
+class UiChooser extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        console.log('connectedCallback')
+    }
+    disconnectedCallback() {
+        console.log('disconnectedCallback')
+
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+
+        console.log('attributeChangedCallback:', name, oldValue, newValue)
+    }
+    static get observedAttributes() { return [] }
+    adoptedCallback() {
+        console.log('adoptedCallback')
+    }
+    #render() {
+        
+    }
+    #makeUl() {
+        this.#makeEl('ul')
+        const ul = document.createElement('ul')
+        return 
+    }
+    #makeEl(name, attrs, children) {
+        const el = document.createElement(name)
+        for (let [k,v] of Object.entries(attrs)) {
+            if ('class'===k) {this.#addClass(el, v)}
+            else {el[key] = v}
+        }
+        this.#addChildren(el, children)
+    }
+    #addClass(el, v) {
+        if ('string'===typeof v || v instanceof String) {v.split(' ').map(c=>el.classList.add(c))}
+        else if(Array.isArray(v) && v.every(V=>'string'===typeof V || V instanceof String)){v.map(V=>el.classList.add(V))}
+        else{throw new TypeError(`vは文字列かその配列であるべきです。`)}
+    }
+    #addChildren(el, v) {
+        if (null===el || undefined===el){return}
+        else if(Array.isArray(v)){el.append(...v)}
+        else{el.append(v)}
+    }
+}
+customElements.define('my-element', UiChooser);
+})();
